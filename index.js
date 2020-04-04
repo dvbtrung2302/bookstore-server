27766924da0798ee1e9fa489549d2a955c3a1a70
dvbtrung2302 <baotrung23022000@gmail.com>
2020-04-04 12:57:57 +0700

    fixed bug

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const productRoute = require('./routes/product.route');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 mongoose.connect(
   process.env.MONGO_URL, 
