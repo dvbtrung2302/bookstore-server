93e04615f9547a82e24ec397fdd19ae36e2e1d7d
dvbtrung2302 <baotrung23022000@gmail.com>
2020-08-08 14:15:37 +0700

    Fix filters books

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,8 +1,20 @@
 const Product = require('../models/product.model');
 
 module.exports.index = async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
+  const { _limit, _category, _keyword } = req.query;
+  let regex = new RegExp(_keyword,'i');
+
+  if ( !_category && !_keyword) {
+    const products = await Product.find().limit(parseInt(_limit));
+    res.json(products);
+  } else {
+    const products = await Product.find(
+      _category && _keyword ? { category: _category, title: regex } : 
+      ( _category ? { category: _category } : { title: regex } )
+      )
+      .limit(parseInt(_limit));
+      res.json(products);
+  }
 }
 
 module.exports.productDetail = async (req, res) => {
